ebed2aa527ac601c08a240aaa2f6ef197d528bee
bin <bin@qq.com>
2016-06-30 16:33:35 -0700

    该克拉克

---
+++ 12/12/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  12
+//
+//  Created by mac on 16/6/30.
+//  Copyright © 2016年 冰帝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 12/12/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  12
+//
+//  Created by mac on 16/6/30.
+//  Copyright © 2016年 冰帝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
